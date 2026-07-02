9e0d0c0cbef9ae6054afca0d26997881763259ef
Swati <swatiguptajnct70@gmail.com>
2026-07-02 13:43:18 +0530

    Updated WhatsApp Agent automation script

--- tests/AIONOS/Agent.spec.js
+++ tests/AIONOS/Agent.spec.js
@@ -10,13 +10,12 @@ test('Login Test', async ({ page }) => {
     await page.getByPlaceholder('Password')
         .fill('Aionos@1234');
 
-    await page.waitForTimeout(2000)
+    await page.waitForTimeout(1000)
     await page.getByRole('button', { name: 'Log In' }).click();
     await page.waitForTimeout(2000);
 
     await page.getByRole('button',{name:'Channel Settings'}).click();
     await page.getByRole('button', { name: 'Add New Account' }).click();
-    await page.waitForTimeout(2000)
 
 
     await page.getByRole('combobox', { name: 'Select Channel' }).click();
@@ -24,22 +23,16 @@ test('Login Test', async ({ page }) => {
     await page.waitForTimeout(1000)
 
     await page.locator('[name="agent_name"]').fill('Swati_Automate');
-    await page.waitForTimeout(1000)
 
     await page.locator('[name="wa_number"]').fill('919999999999');
-    await page.waitForTimeout(1000)
 
     await page.locator('[name="wabaid"]').fill('123456789');
-    await page.waitForTimeout(1000)
 
     await page.locator('[name="phone_number_id"]').fill('123456789');
-    await page.waitForTimeout(1000)
 
     await page.locator('[name="tier"]').fill('999'); 
-    await page.waitForTimeout(1000)  
 
     await page.locator('[name="wa_access_token"]').fill('9045803495ioruoieuroeiureiwoie')
-    await page.waitForTimeout(1000)
 
     await page.locator('button[type="submit"]').click();
     await page.waitForTimeout(5000)
